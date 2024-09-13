e58d748b6433c8c2c76d8115c08d8f453e90a3cf
MAULIZA APRILIA <118743543+MaulizaAprilia@users.noreply.github.com>
2024-09-14 00:34:25 +0700

    Second Commit

--- PBJ2/app.js
+++ PBJ2/app.js
@@ -1,20 +1,17 @@
 //a. import core module node.js
 const fs = require('fs')
-//fs.writeFileSync('catatan.txt', 'Nama Saya Mauliza Aprilia')
+//fs.writeFileSync('catatan.txt', 'Nama Saya Mauliza Aprilia\n')
 fs.appendFileSync('catatan.txt', 'Saya tinggal di Sago')
 
 
 //b. import file pada node.js
 const catatan = require('./catatan.js')
-const pesan = catatan()
-console.log(pesan)
-
-
+const pesan1 = catatan()
+console.log(pesan1)
 
 //c. import npm (Node Package Manager) pada node.js
 const validator = require('validator')
 const ambilCatatan = require('./catatan.js')
-
-console.log(pesan)
+const pesan2 = ambilCatatan()
+console.log(pesan2)
 console.log(validator.isURL('https://mauliza.com'))
-
