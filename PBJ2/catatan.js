0419eb36eb6d9bce71c0597c563c94215acc5475
MAULIZA APRILIA <118743543+MaulizaAprilia@users.noreply.github.com>
2024-09-26 22:46:00 +0700

    add ulang import core module node.js

--- PBJ2/catatan.js
+++ PBJ2/catatan.js
@@ -1,98 +1,103 @@
-const fs = require('fs');
-const chalk = require('chalk');
+//a. import core module node.js
+const fs = require('fs')
+fs.writeFileSync('catatan.txt', 'Nama Saya Mauliza Aprilia\n')
+fs.appendFileSync('catatan.txt', 'Saya tinggal di Sago')
 
-// Mengambil catatan dasar
-const ambilCatatan = function () {
-    return 'Ini Catatan Mauliza Aprilia ...';
-};
 
-// Menambahkan catatan baru
-const tambahCatatan = function (judul, isi) {
-    const catatan = muatCatatan(); // Memuat catatan yang ada
-    const catatanGanda = catatan.filter(function (note) {
-        return note.judul === judul; // Pastikan membandingkan judul
-    });
+// const chalk = require('chalk');
 
-    // Jika catatan dengan judul yang sama tidak ada
-    if (catatanGanda.length === 0) {
-        catatan.push({
-            judul: judul,
-            isi: isi
-        });
-        simpanCatatan(catatan); // Simpan catatan yang baru
-        console.log('Catatan baru ditambahkan!');
-    } else {
-        console.log('Judul catatan telah dipakai');
-    }
-};
+// // Mengambil catatan dasar
+// const ambilCatatan = function () {
+//     return 'Ini Catatan Mauliza Aprilia ...';
+// };
 
-// Menghapus catatan berdasarkan judul
-const hapusCatatan = function (judul) {
-    const catatan = muatCatatan(); // Memuat catatan yang ada
-    const catatanUntukDisimpan = catatan.filter(function (note) {
-        return note.judul !== judul; // Menghapus catatan yang judulnya sama
-    });
+// // Menambahkan catatan baru
+// const tambahCatatan = function (judul, isi) {
+//     const catatan = muatCatatan(); // Memuat catatan yang ada
+//     const catatanGanda = catatan.filter(function (note) {
+//         return note.judul === judul; // Pastikan membandingkan judul
+//     });
 
-    if (catatan.length > catatanUntukDisimpan.length) {
-        console.log(chalk.green.inverse('Catatan dihapus!'));
-        simpanCatatan(catatanUntukDisimpan); // Simpan catatan yang tersisa
-    } else {
-        console.log(chalk.red.inverse('Catatan tidak ditemukan!'));
-    }
-};
+//     // Jika catatan dengan judul yang sama tidak ada
+//     if (catatanGanda.length === 0) {
+//         catatan.push({
+//             judul: judul,
+//             isi: isi
+//         });
+//         simpanCatatan(catatan); // Simpan catatan yang baru
+//         console.log('Catatan baru ditambahkan!');
+//     } else {
+//         console.log('Judul catatan telah dipakai');
+//     }
+// };
 
-// Menyimpan catatan ke file
-const simpanCatatan = function (catatan) { 
-    const dataJSON = JSON.stringify(catatan); 
-    fs.writeFileSync('catatan.json', dataJSON); 
-};
+// // Menghapus catatan berdasarkan judul
+// const hapusCatatan = function (judul) {
+//     const catatan = muatCatatan(); // Memuat catatan yang ada
+//     const catatanUntukDisimpan = catatan.filter(function (note) {
+//         return note.judul !== judul; // Menghapus catatan yang judulnya sama
+//     });
 
-// Memuat catatan dari file
-const muatCatatan = function () { 
-    try { 
-        const dataBuffer = fs.readFileSync('catatan.json'); 
-        const dataJSON = dataBuffer.toString(); 
-        return JSON.parse(dataJSON); 
-    } catch (e) { 
-        return []; // Mengembalikan array kosong jika file tidak ditemukan
-    } 
-};
+//     if (catatan.length > catatanUntukDisimpan.length) {
+//         console.log(chalk.green.inverse('Catatan dihapus!'));
+//         simpanCatatan(catatanUntukDisimpan); // Simpan catatan yang tersisa
+//     } else {
+//         console.log(chalk.red.inverse('Catatan tidak ditemukan!'));
+//     }
+// };
 
-// Menampilkan semua catatan
-const listCatatan = function () {
-    const catatan = muatCatatan(); // Memuat catatan
-    if (catatan.length > 0) {
-        console.log('Daftar Catatan:');
-        catatan.forEach((note, index) => {
-            console.log(`${index + 1}. Judul: ${note.judul}, Isi: ${note.isi}`);
-        });
-    } else {
-        console.log('Tidak ada catatan yang ditemukan.');
-    }
-};
+// // Menyimpan catatan ke file
+// const simpanCatatan = function (catatan) { 
+//     const dataJSON = JSON.stringify(catatan); 
+//     fs.writeFileSync('catatan.json', dataJSON); 
+// };
 
-// Membaca catatan berdasarkan judul
-const readCatatan = function (judul) {
-    const catatan = muatCatatan(); // Memuat catatan
-    const catatanDitemukan = catatan.find(function (note) {
-        return note.judul === judul; // Mencari catatan dengan judul yang diberikan
-    });
+// // Memuat catatan dari file
+// const muatCatatan = function () { 
+//     try { 
+//         const dataBuffer = fs.readFileSync('catatan.json'); 
+//         const dataJSON = dataBuffer.toString(); 
+//         return JSON.parse(dataJSON); 
+//     } catch (e) { 
+//         return []; // Mengembalikan array kosong jika file tidak ditemukan
+//     } 
+// };
 
-    if (catatanDitemukan) {
-        console.log('Catatan Ditemukan:');
-        console.log(`Judul: ${catatanDitemukan.judul}`);
-        console.log(`Isi: ${catatanDitemukan.isi}`);
-    } else {
-        console.log(chalk.red.inverse('Catatan tidak ditemukan!'));
-    }
-};
+// // Menampilkan semua catatan
+// const listCatatan = function () {
+//     const catatan = muatCatatan(); // Memuat catatan
+//     if (catatan.length > 0) {
+//         console.log('Daftar Catatan:');
+//         catatan.forEach((note, index) => {
+//             console.log(`${index + 1}. Judul: ${note.judul}, Isi: ${note.isi}`);
+//         });
+//     } else {
+//         console.log('Tidak ada catatan yang ditemukan.');
+//     }
+// };
 
-// Ekspor fungsi
-module.exports = { 
-    ambilCatatan, 
-    tambahCatatan,
-    hapusCatatan, 
-    listCatatan,
-    muatCatatan,
-    readCatatan// Tambahkan fungsi bacaCatatan ke ekspor
-};
+// // Membaca catatan berdasarkan judul
+// const readCatatan = function (judul) {
+//     const catatan = muatCatatan(); // Memuat catatan
+//     const catatanDitemukan = catatan.find(function (note) {
+//         return note.judul === judul; // Mencari catatan dengan judul yang diberikan
+//     });
+
+//     if (catatanDitemukan) {
+//         console.log('Catatan Ditemukan:');
+//         console.log(`Judul: ${catatanDitemukan.judul}`);
+//         console.log(`Isi: ${catatanDitemukan.isi}`);
+//     } else {
+//         console.log(chalk.red.inverse('Catatan tidak ditemukan!'));
+//     }
+// };
+
+// // Ekspor fungsi
+// module.exports = { 
+//     ambilCatatan, 
+//     tambahCatatan,
+//     hapusCatatan, 
+//     listCatatan,
+//     muatCatatan,
+//     readCatatan// Tambahkan fungsi bacaCatatan ke ekspor
+// };
